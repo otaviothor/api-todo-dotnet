e6cab347c44326887cc81c4b739b31e5e05d5a6f
otavio <otaviosilva2632@gmail.com>
2021-11-07 20:55:09 -0300

    feat: create todo model

---
+++ Models/Todo.cs
@@ -0,0 +1,9 @@
+namespace Todo.Models
+{
+    public class Todo
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public bool Done { get; set; }
+    }
+}
