b1c68436e339f7311228e56572ca0360c0651c02
Otávio Silva <otaviosilva2632@gmail.com>
2021-11-08 10:13:42 -0300

    feat: create put and delete methods in todo controller

--- Controllers/TodoController.cs
+++ Controllers/TodoController.cs
@@ -84,5 +84,58 @@ namespace Todo.Controllers
                 return BadRequest(e.Message);
             }
         }
+
+        [HttpPut("todos/{id}")]
+        public async Task<IActionResult> PutAsync(
+            [FromServices] AppDbContext context,
+            [FromBody] CreateTodoViewModel model,
+            [FromRoute] int id)
+        {
+            try
+            {
+                if (!ModelState.IsValid) return BadRequest();
+
+                var todo = await context
+                    .Todos
+                    .FirstOrDefaultAsync(x => x.Id == id);
+
+                if (todo == null) NotFound();
+
+                todo.Title = model.Title;
+
+                context.Todos.Update(todo);
+                await context.SaveChangesAsync();
+
+                return Ok(todo);
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+
+        [HttpDelete("todos/{id}")]
+        public async Task<IActionResult> DeleteAsync(
+            [FromServices] AppDbContext context,
+            [FromRoute] int id)
+        {
+            try
+            {
+                var todo = await context
+                    .Todos
+                    .FirstOrDefaultAsync(x => x.Id == id);
+
+                if (todo == null) NotFound();
+
+                context.Todos.Remove(todo);
+                await context.SaveChangesAsync();
+
+                return Ok();
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
     }
 }
