10a7bad85c8aac005ad9fc19f7f19a81dbd8c78f
Otávio Silva <otaviosilva2632@gmail.com>
2021-11-08 09:50:34 -0300

    feat: add try catch

--- Controllers/TodoController.cs
+++ Controllers/TodoController.cs
@@ -17,32 +17,45 @@ namespace Todo.Controllers
         [HttpGet]
         [Route("todos")]
         public async Task<IActionResult> GetAsync(
-            [FromServices] AppDbContext context
-        )
+            [FromServices] AppDbContext context)
         {
-            var todos = await context
-                .Todos
-                .AsNoTracking()
-                .ToListAsync();
+            try
+            {
+                var todos = await context
+                    .Todos
+                    .AsNoTracking()
+                    .ToListAsync();
 
-            return Ok(todos);
+                return Ok(todos);
+
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
         }
 
         [HttpGet]
         [Route("todos/{id}")]
         public async Task<IActionResult> GetByIdAsync(
             [FromServices] AppDbContext context,
-            [FromRoute] int id
-        )
+            [FromRoute] int id)
         {
-            var todo = await context
-                .Todos
-                .AsNoTracking()
-                .FirstOrDefaultAsync(x => x.Id == id);
+            try
+            {
+                var todo = await context
+                    .Todos
+                    .AsNoTracking()
+                    .FirstOrDefaultAsync(x => x.Id == id);
 
-            if (todo == null) return NotFound();
+                if (todo == null) return NotFound();
 
-            return Ok(todo);
+                return Ok(todo);
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
         }
 
         [HttpPost("todos")]
@@ -68,7 +81,6 @@ namespace Todo.Controllers
             }
             catch (Exception e)
             {
-
                 return BadRequest(e.Message);
             }
         }
